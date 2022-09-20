b03ee09f512bd0a5734510791e35918e38299f98
anaves <anaves@gmail.com>
2022-09-20 20:17:08 -0300

    Aula 20/09/2022

--- arquivos/manipula_json.py
+++ arquivos/manipula_json.py
@@ -1,16 +1,35 @@
 import json
 
-dados = None
-with open("cliente.json", encoding="utf-8") as arquivo:
-    dados = json.load(arquivo)
+def abrir_arquivo(url):
+    dados = None
+    with open(url, encoding="utf-8") as arquivo:
+        dados = json.load(arquivo)       
+    return dados
 
-print(len(dados))
+def informacoes(reg):
+    print(f'Nome: {reg["nome"]}')
+    print(f'CPF: {reg["cpf"]}')
+    print(f'e-mail: {reg["email"]}')
+    print(f'endereco: {reg["endereco"]}')
+    print(f'telefone: {reg["telefone"]}')
+    print(f'data nascimento: {reg["dataNascimento"]}')
+    print('-'*30)
 
-print('Exemplo de busca')
-for registro in dados:
-    #print(f"cpf: {registro['cpf']}")    
-    if '751.545.720-17' in registro['cpf']:
-        print(f"nome: {registro['nome']}")
+def listar_todos(dados):
+    print('--- Listar todos ---')
+    for reg in dados:
+        informacoes(reg)
 
-
+def listar_por_cpf(dados, cpf):
+    print(f'Buscar CPF: {cpf}')
+    for reg in dados:
+        if reg['cpf'] == cpf:
+            informacoes(reg)
+            return True
+    print('Nao encontrado')
+    return False
     
+if __name__== "__main__":
+    base_dados = abrir_arquivo("./files/cliente.json")
+    listar_todos(base_dados)
+    listar_por_cpf(base_dados,'751.535.720-17')
